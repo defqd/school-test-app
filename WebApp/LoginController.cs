24797d472f79d15297b9720f3a01db50c4075984
defqd <DefQD@yandex.ru>
2023-05-21 18:42:36 +0300

    feat: generate auth cookie for user 'userName' with external id(TODO 1)

--- WebApp/LoginController.cs
+++ WebApp/LoginController.cs
@@ -17,13 +17,24 @@ namespace WebApp
             _db = db;
         }
 
-        [HttpPost("sign-in")]
+        [HttpPost("sign-in/{userName}")]
         public async Task<IActionResult> Login(string userName)
         {
             var account = await _db.FindByUserNameAsync(userName);
             if (account != null)
             {
                 //TODO 1: Generate auth cookie for user 'userName' with external id
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, account.ExternalId),
+                    new Claim(ClaimTypes.Role, account.Role)
+                };
+
+                var userId = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(new ClaimsPrincipal(userId));
+
+                return Ok();
             }
             //TODO 2: return 404 if user not found
             return NotFound();
