1ee20a84650521908d56991ca4ed7e5abdd6626e
defqd <DefQD@yandex.ru>
2023-05-21 18:12:38 +0300

    fix: use MVC without endpoint routing

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -15,7 +15,8 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
             services.AddSingleton<IAccountDatabase, AccountDatabaseStub>();
             services.AddSingleton<IAccountCache, AccountCache>();
         }
